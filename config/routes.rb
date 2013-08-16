5bb9d0395fa01e6f73783abfc46721eeae8434f4
demibenari <demibenari@gmail.com>
2013-08-16 10:11:05 +0300

    Changing the main page of the application

--- config/routes.rb
+++ config/routes.rb
@@ -70,7 +70,7 @@ Kicksight::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'main#index'
 
   # See how all your routes lay out with "rake routes"
 
