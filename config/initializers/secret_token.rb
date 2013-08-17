a55cbd59af0e81cb807f00532170130e48cea047
Demi Ben-Ari <demi.benari@gmail.com>
2013-08-17 17:29:22 +0300

    Adding all the Rails4 Project Files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kicksight::Application.config.secret_key_base = 'c836282012a6872369afbbcfe6c5d571a8fddc4df279c14844d39f5471ed7e36a4be1ae14b4b69deb958e40ebb9e9d2acd408fa50f5ef94bbab1109a7a87948f'
