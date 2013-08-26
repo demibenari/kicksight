f0b662b16ff81c44013610f5d4b3c13ae1d1762c
Demi Ben-Ari <demi.benari@gmail.com>
2013-08-26 22:24:03 +0300

    trendline and dailies are working

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,5 +5,17 @@ class Project < ActiveRecord::Base
   has_many :dailies
   has_many :push_dates, :through => :dailies
 
+  def as_json(options = { })
+    h = super(options)
 
+    current_pledged = Daily.where(project_id: id)
+    last_daily = current_pledged.last()
+
+    if (!last_daily.nil?)
+      h[:current]   = last_daily.read_attribute('amount_pledged')
+    else
+      puts 'The kick id of the bad project is: ' + kick_id
+    end
+    h
+end
 end
